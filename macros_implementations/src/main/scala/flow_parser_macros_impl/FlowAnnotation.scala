e90d6069129171029d73465ccf21b5b4a9e90fd0
Kim Stebel <kim.stebel@ovoenergy.com>
2017-01-08 13:08:45 +0000

    refactoring, adding field names and types

--- macros_implementations/src/main/scala/flow_parser_macros_impl/FlowAnnotation.scala
+++ macros_implementations/src/main/scala/flow_parser_macros_impl/FlowAnnotation.scala
@@ -10,67 +10,83 @@ class FlowParser extends StaticAnnotation {
 
 object FlowParser {
   def impl(c: Context)(annottees: c.Expr[Any]*): c.Expr[Any] = {
-    import c.universe._
 
-    val tree = {
-      annottees.map(_.tree).toList match {
-        case q"$mods object $tpname extends $parent with ..$parents { $self => ..$stats }" :: Nil => {
-          val flowType = tpname.toString
+    def classAndCompanion(group: FlowGroup): Seq[c.Tree] = {
+      import c.universe._
+      
+      val className = s"Group${group.id}"
+      
+      val columnParams = group.columns.map { 
+        case (colName, fieldType) => q"${TermName(colName)}: ${TypeName(fieldType.typeName)}"
+      }
+      
+      val subGroupParams: IndexedSeq[Tree] = group.subGroups.toIndexedSeq.map(sg => {
+        val tn = TermName(s"g${sg.id}")
+        val tpN = TypeName(s"Group${sg.id}")
+        q"$tn: Seq[$tpN]"
+      })
+      
+      val params = columnParams ++ subGroupParams
+
+      val groupIdStr = Literal(Constant(group.id.toString))
+      val subGroupConstParams = IndexedSeq.fill(group.subGroups.size)(q"Seq.empty")
+      val fields = group.columns.map { case (name, _) => pq"""${TermName(name)}""" }
+      val rFields = group.columns.map { case (name,_) => q"""${TermName(name)}""" }
+      
+      println(fields)
+      val constParams = rFields ++ subGroupConstParams
+      
+      Seq(
+        q"""case class ${TypeName(className)}(..$params)""",
+        q"""object ${TermName(className)} {
+              val parser = {
+                import fastparse.all._
+                import scala.collection.mutable.ArrayBuffer
+                import flow_parser_macros_impl.Parsing
+  
+                val lp = Parsing.lineParser($groupIdStr, 3)
+                lp.map {
+                  case ArrayBuffer(..$fields) => ${TermName(className)}(..$constParams)
+                  case x => {println("couldn't match " + x + ". This is probably a bug in the code generator"); ???}
+                }
+              }
+            }
+         """
+      ) 
+    }
+    
+    def annotateObject(o: c.Tree): c.Tree = {
+      import c.universe._
+      o match {
+        case q"$mods object $name extends $parent with ..$parents { $self => ..$stats }" => {
+          val flowType = name.toString
           val groups = FlowDescription(flowType).toSeq.flatMap(_.allGroups)
-          val classDefs = groups.flatMap(group => {
-            val className = TypeName(s"Group${group.id}")
-            val classNameTerm = TermName(s"Group${group.id}")
-            val termNames = (1 to group.columns).map(n => TermName(s"field$n"))
-            val columnParams = termNames.map(tn => q"$tn: String")
-            val subGroupParams: IndexedSeq[Tree] = group.subGroups.toIndexedSeq.map(sg => {
-              val tn = TermName(s"g${sg.id}")
-              val tpN = TypeName(s"Group${sg.id}")
-              q"$tn: Seq[$tpN]"
-            })
-            val params = columnParams ++ subGroupParams
-            
-            val groupIdStr = Literal(Constant(group.id.toString))
-            val subGroupConstParams = IndexedSeq.fill(group.subGroups.size)(q"Seq.empty")
-            val fields = (1 to group.columns).map(n => pq"""${TermName("matchField" + n)}""")
-            val rFields = (1 to group.columns).map(n => q"""${TermName("matchField" + n)}""")
-            println(fields)
-            val constParams = rFields ++ subGroupConstParams
-            Seq(
-              q"""case class $className(..$params) {
-                    
-                  }""", q"""
-                  object $classNameTerm {
-                    val parser = {
-                      import fastparse.all._
-                      import scala.collection.mutable.ArrayBuffer
-                      import flow_parser_macros_impl.Parsing
-                      
-                      val lp = Parsing.lineParser($groupIdStr, 3)
-                      lp.map {
-                        case ArrayBuffer(..$fields) => $classNameTerm(..$constParams)
-                        case x => {println("couldn't match " + x + ". This is probably a bug in the code generator"); ???}
-                      }
-                    }
-                  }
-               """
-            )
-          })
-          
-          q"""$mods object $tpname extends $parent with ..$parents {
+          val classDefs = groups.flatMap(g => classAndCompanion(g))
+
+          q"""$mods object $name extends $parent with ..$parents {
                 $self =>
-              
+
                 def flowType: String = $flowType
-            
+
                 ..$classDefs
-            
+
                 ..$stats
               }"""
+
         }
+      }
+    }
+    
+    val tree:c.Tree = {
+      annottees.map(_.tree).toList match {
+        case a :: Nil => annotateObject(a)
         case _ => c.abort(c.enclosingPosition, "Annotation @FlowParser can only be used with objects")
       }
     }
     c.Expr[Any](tree)
   }
+  
+  
 }
 
 object Parsing {
--- macros_implementations/src/main/scala/flow_parser_macros_impl/flowDescriptions.scala
+++ macros_implementations/src/main/scala/flow_parser_macros_impl/flowDescriptions.scala
@@ -1,21 +1,28 @@
 package flow_parser_macros_impl
 
 object FlowDescription {
-  def apply(name: String): Option[Group] = name match {
+  def apply(name: String): Option[FlowGroup] = name match {
     case "D188" => Some(d188)
     case _ => None
   }
   
-  val d188: Group = Group(386, "Customer Payment Details", 15, Seq(
-                      Group(387, "MeterRegisterDetailPerMeter", 3, Seq.empty)
+  val d188: FlowGroup = FlowGroup(386, "Customer Payment Details", (1 to 15).map(n => ("field" + n) -> StringField), Seq(
+                      FlowGroup(387, "MeterRegisterDetailPerMeter", (1 to 3).map(n => ("field" + n) -> StringField), Seq.empty)
                     ))
   
   //case class G386(f1 .. f15: String, g387s: Seq[G387])
   
 }
 
-case class Group(id: Int, description:String, columns: Int, subGroups: Seq[Group]) {
-  def allGroups:Seq[Group] = this :: subGroups.toList.flatMap(_.allGroups)
+sealed trait FieldType {
+  def typeName: String
+}
+object StringField extends FieldType {
+  val typeName = "String"
+}
+
+case class FlowGroup(id: Int, description:String, columns: Seq[(String, FieldType)], subGroups: Seq[FlowGroup]) {
+  def allGroups:Seq[FlowGroup] = this :: subGroups.toList.flatMap(_.allGroups)
 }
 
 
